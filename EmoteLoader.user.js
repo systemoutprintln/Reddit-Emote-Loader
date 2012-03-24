136324dc4e77cb18b9028452042c636a480c94a3
System <sys.out.println@gmail.com>
2012-03-23 20:37:52 -0400

    0.8.6 Emote sheet by sub p2

--- EmoteLoader.user.js
+++ EmoteLoader.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           Reddit Emote Loader
 // @namespace      http://www.reddit.com/r/RedditEmoteLoader
-// @version        0.8.5
+// @version        0.8.6
 // @include        http://www.reddit.com/*
 // @include        http://reddit.com/*
 // @include        http://*.reddit.com/*
@@ -461,7 +461,7 @@ function openEmotePage()
 	
 	over.appendChild(sub_disp);
 
-	addEmotes(0, sub_disp);
+	//addEmotes(0, sub_disp);
 	
 
 }
