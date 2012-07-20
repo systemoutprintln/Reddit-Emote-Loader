e9faf3ef7cd190c9a989c596381b1367e371f18a
System <sys.out.println@gmail.com>
2012-07-20 10:27:48 -0400

    extra CSS

--- EmoteLoader.user.js
+++ EmoteLoader.user.js
@@ -517,6 +517,15 @@ function addRules(sub)
 function ExtraCSS() 
 {
     var ecssar = ExCss.split("\n");
+	
+	if(chrome)
+	{
+	   ecssar += ecssCH.split("\n");
+	}
+	else
+	{
+	    ecssar += ecssFF.split("\n");
+	}
 
 	for(i=0; i < ecssar.length; i++)
 	{
